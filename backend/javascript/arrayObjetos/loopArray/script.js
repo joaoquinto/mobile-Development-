c8d59481f48a664c6e414e5be964badfa45d4838
joaov <joaovictoroliveirapereira6522@gmail.com>
2020-09-25 22:56:18 -0300

    :art: melhorando e incrementando novos atributos

--- backend/javascript/arrayObjetos/loopArray/script.js
+++ backend/javascript/arrayObjetos/loopArray/script.js
@@ -7,7 +7,7 @@ let cadastro = [
     {
         name: "Pinxoy",
         dataNascimento: "27/10/1999",
-        cpf: null
+        cpf: "056.066.555-08"
 
     },
     {
@@ -18,20 +18,34 @@ let cadastro = [
 ]
 
 now = new Date
-
 for (let index = 0; index < cadastro.length; index++) {
     cadastro[index].dataCadastro = now.getDate() + "/" + `${now.getMonth() + 1}` + "/" + now.getFullYear()
+    cadastro[index].horaCadastro = now.getHours() + ":" + now.getMinutes()
+    cadastro[index].numeroCadastro = index
 
 }
-for (let index = 0; index < cadastro.length; index++) {
-    const element = cadastro[index];
-    if (element.cpf == null) {
-        console.log(`O seu nome é ${element.name} nasceu ${element.dataNascimento} data de cadastro: ${element.dataCadastro}`);
-        console.log(`Por favor ${element.name} adicione o seu CPF na próxima atualização!`);
-    } else {
-        console.log(`O seu nome é ${element.name} com o CPF: ${element.cpf} e nasceu ${element.dataNascimento}`);
-        console.log(`Obrigado Sr(a) ${element.name} pelo o seu ingresso ao nosso cadastro! data de cadastro: ${element.dataCadastro}`);
+
+let showCadastrado = () => {
+    for (let index = 0; index < cadastro.length; index++) {
+        const element = cadastro[index];
+        if (element.cpf == null) {
+            console.log(`Nome:${element.name}\nNasceu ${element.dataNascimento}\nHora do cadastro:${element.horaCadastro} \nData de cadastro: ${element.dataCadastro} `);
+            console.log(`Por favor ${element.name} adicione o seu CPF na próxima atualização!\n`);
+        } else {
+            console.log(`Nome:${element.name}\nCPF:${element.cpf}\nData de nascimento: ${element.dataNascimento}`);
+            console.log(`Obrigado Sr(a) ${element.name} pelo o seu ingresso ao nosso cadastro!\nHora do cadastro:${element.horaCadastro}\nData de cadastro: ${element.dataCadastro}! \n`);
+        }
     }
 }
 
+showCadastrado()
 
+// Update
+cadastro[2].cpf = "015.086.038-03"
+
+//delete
+delete cadastro[2].cpf
+// create
+cadastro[2].cpf = "111.111.111-03"
+console.log("-------------------------------------------------");
+showCadastrado()
