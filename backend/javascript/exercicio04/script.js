0b28206db5b09d316f4c4a24ac6480009f421e7c
joaoquinto <45634973+joaoquinto@users.noreply.github.com>
2020-10-05 00:46:25 -0300

    Update script.js

--- backend/javascript/exercicio04/script.js
+++ backend/javascript/exercicio04/script.js
@@ -4,8 +4,5 @@ var ternario = (sexo == "Masculino" || sexo == "Feminino" ? `O sexo é: ${sexo}`
 
 var ternario01 = (sexo == "Masculino" && idade == 20 ? `O gênero é ${sexo} e a idade é ${idade}  e você está no enquadramento` : " O seu gênero e a sua idade não se enquadram");
 
-var ternario02 = (sexo != "Masculino" && idade == 20 ?  ` O gênero ${sexo} está correto` : "troque seu gênero seu porra" )
-
 console.log(ternario);
 console.log(ternario01);
-console.log(ternario02);
